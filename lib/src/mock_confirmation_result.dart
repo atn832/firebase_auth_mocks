6e72e2448149c36d583e70d8f9d15b87b726795b
Anh Tuan <atn832@gmail.com>
2021-03-13 12:09:44 +0900

    make onConfirm required

--- lib/src/mock_confirmation_result.dart
+++ lib/src/mock_confirmation_result.dart
@@ -2,12 +2,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mockito/mockito.dart';
 
 class MockConfirmationResult extends Mock implements ConfirmationResult {
-  Function? onConfirm;
+  Function onConfirm;
 
-  MockConfirmationResult({this.onConfirm});
+  MockConfirmationResult({required this.onConfirm});
 
   @override
   Future<UserCredential> confirm(String verificationCode) {
-    return onConfirm!();
+    return onConfirm();
   }
 }
