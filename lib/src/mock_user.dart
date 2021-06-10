750c61d07244476d5d7218c7d60a5efbeba90be2
mazzonem <maxime.mazzone@gmail.com>
2021-06-10 07:20:50 +0200

    Non-nullable for Mock user

--- lib/src/mock_user.dart
+++ lib/src/mock_user.dart
@@ -2,32 +2,41 @@ import 'package:equatable/equatable.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class MockUser with EquatableMixin implements User {
-  final bool? _isAnonymous;
+  final bool _isAnonymous;
+  final bool _isEmailVerified;
   final String _uid;
   final String? _email;
   final String? _displayName;
   final String? _phoneNumber;
   final String? _photoURL;
   final String? _refreshToken;
+  final UserMetadata? _metadata;
 
   MockUser({
-    bool? isAnonymous = false,
+    bool isAnonymous = false,
+    bool isEmailVerified = true,
     String uid = 'some_random_id',
     String? email,
     String? displayName,
     String? phoneNumber,
     String? photoURL,
     String? refreshToken,
+    UserMetadata? metadata,
   })  : _isAnonymous = isAnonymous,
+        _isEmailVerified = isEmailVerified,
         _uid = uid,
         _email = email,
         _displayName = displayName,
         _phoneNumber = phoneNumber,
         _photoURL = photoURL,
-        _refreshToken = refreshToken;
+        _refreshToken = refreshToken,
+        _metadata = metadata;
 
   @override
-  bool get isAnonymous => _isAnonymous!;
+  bool get isAnonymous => _isAnonymous;
+
+  @override
+  bool get emailVerified => _isEmailVerified;
 
   @override
   String get uid => _uid;
@@ -53,6 +62,9 @@ class MockUser with EquatableMixin implements User {
   }
 
   @override
+  UserMetadata get metadata => _metadata!;
+
+  @override
   List<Object?> get props => [
         _isAnonymous,
         _uid,
@@ -61,6 +73,7 @@ class MockUser with EquatableMixin implements User {
         _phoneNumber,
         _photoURL,
         _refreshToken,
+        metadata,
       ];
 
   @override
